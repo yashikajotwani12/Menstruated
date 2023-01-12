d51929738a47554e7e01071034876594d44f68f0
yashikajotwani12 <yashikajotwani12@gmail.com>
2023-01-12 20:09:47 +0530

    deployment

--- StainStrong/wsgi.py
+++ StainStrong/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StainStrong.settings')
 
 application = get_wsgi_application()
-
-app = application
